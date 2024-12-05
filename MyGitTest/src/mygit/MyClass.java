e7db343e8c40dd41ff35c5694c83e1a32651a09f
admin <admin@DESKTOP-I6HUGBR>
2024-12-05 13:57:10 +0900

    [feat] 두번째 줄 추가햇음

--- MyGitTest/src/mygit/MyClass.java
+++ MyGitTest/src/mygit/MyClass.java
@@ -2,6 +2,7 @@ package mygit;
 
 public class MyClass {
 	public static void main(String[] args) {
-		System.out.println("HellowGit!");
+		System.out.println("Hello Git!");
+		System.out.println("This is the 2nd version!");
 	}
 }
